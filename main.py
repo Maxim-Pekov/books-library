40e9b4dff18152761cce14df326c9f4f47407893
Maxim-Pekov <64793556+Maxim-Pekov@users.noreply.github.com>
2022-10-01 16:57:06 +0300

    download 10 books to the books directory

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 from pprint import pprint
 
 import requests
+import pathlib
+
+
+from pathlib import Path
+
+
+directory_path = 'books'
+
+
+def save_book(book, title, id, directory_path=directory_path):
+    """Создает директорию {directory_path} в корне проекта и сохраняет туда переданный файл"""
+
+    pathlib.Path(directory_path).mkdir(parents=True, exist_ok=True)
+    outpath = Path() / directory_path / f'{title}_{id}.txt'
+    with open(outpath, 'w', encoding='utf-8') as file:
+        file.write(book)
+
+
+def get_books(url, count):
+    for _ in range(1, count):
+        params = {'id': _}
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        title = 'book'
+        save_book(response.text, title, _)
 
 
 def main():
     url = "https://tululu.org/txt.php"
-    params = {'id': 32168}
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    filename = 'dvmn.txt'
-    with open(filename, 'w', encoding='utf-8') as file:
-        file.write(response.text)
+    get_books(url, 10)
 
 
 if __name__ == '__main__':

98e8746cab18905aeccc7f55054d0ec42c832a10
Maxim-Pekov <max.pekov@gmail.com>
2022-10-16 15:51:07 +0300

    download books with titles

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-from pprint import pprint
-
 import requests
-import pathlib
+import pathlib, os
 
+from pprint import pprint
 from bs4 import BeautifulSoup as bs
 from pathlib import Path
+from pathvalidate import sanitize_filename
+from urllib.parse import urlsplit
 
 
-directory_path = 'books'
+def get_title(url, book_id):
+    """Парсит сайт возвращая название книги по ее id"""
+
+    url_parse = urlsplit(url)
+    base_url = url_parse._replace(path="").geturl()
+    link_book_url = os.path.join(base_url, f'b{book_id}')
+    response = requests.get(link_book_url)
+    soup = bs(response.text, 'lxml')
+    book_info = soup.body.find('div', id='content').h1.text
+    file_name = book_info.split('::')[0].strip()
+    title = sanitize_filename(file_name)
+    autor = book_info.split('::')[1].strip()
+    return title
 
 
-def save_book(book, title, id, directory_path=directory_path):
+def save_book(book, title, folder='books/'):
     """Создает директорию {directory_path} в корне проекта и сохраняет туда переданный файл"""
 
-    pathlib.Path(directory_path).mkdir(parents=True, exist_ok=True)
-    outpath = Path() / directory_path / f'{title}_{id}.txt'
-    with open(outpath, 'w', encoding='utf-8') as file:
+    pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
+    directory_path = Path() / folder / f'{title}.txt'
+    with open(directory_path, 'w', encoding='utf-8') as file:
         file.write(book)
+    return directory_path
 
 
 def check_for_redirect(response):
@@ -33,23 +47,13 @@ def get_books(url, count):
         response.raise_for_status()
         if check_for_redirect(response):
             continue
-        title = 'book'
-        save_book(response.text, title, _)
+        title = get_title(url, _)
+        save_book(response.text, title)
 
 
 def main():
-    # url = "https://tululu.org/txt.php"
-    url = "https://tululu.org/b1/"
-    # get_books(url, 10)
-    response = requests.get(url)
-    soup = bs(response.text, 'lxml')
-    # print(soup.body.prettify())
-
-    book_info = soup.body.find('div', id='content').h1.text
-    title = f"Заголовок: {book_info.split('::')[0].strip()}"
-    autor = f"Автор: {book_info.split('::')[1].strip()}"
-    print(title)
-    print(autor)
+    url = "https://tululu.org/txt.php"
+    get_books(url, 10)
 
 
 if __name__ == '__main__':

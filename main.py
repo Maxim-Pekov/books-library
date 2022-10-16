4f33fb47da8121600ef7dc462f64dad2ee5fddbe
Maxim-Pekov <max.pekov@gmail.com>
2022-10-16 18:12:03 +0300

    download book covers

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from pprint import pprint
 from bs4 import BeautifulSoup as bs
 from pathlib import Path
 from pathvalidate import sanitize_filename
-from urllib.parse import urlsplit
+from urllib.parse import urlsplit, urljoin
 
 
-def get_title(url, book_id):
+def get_books_info(url, book_id):
     """Парсит сайт возвращая название книги по ее id"""
 
     url_parse = urlsplit(url)
@@ -17,13 +17,16 @@ def get_title(url, book_id):
     response = requests.get(link_book_url)
     soup = bs(response.text, 'lxml')
     book_info = soup.body.find('div', id='content').h1.text
+    image_url = soup.body.find('div', class_='bookimage').img['src']
+    image = urljoin(base_url, image_url)
+    print(image)
     file_name = book_info.split('::')[0].strip()
     title = sanitize_filename(file_name)
     autor = book_info.split('::')[1].strip()
-    return title
+    return title, image
 
 
-def save_book(book, title, id='', folder='books/'):
+def save_book(book, title, id='', folder='static/books/'):
     """Создает директорию {directory_path} в корне проекта и сохраняет туда переданный файл"""
 
     pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
@@ -32,6 +35,15 @@ def save_book(book, title, id='', folder='books/'):
         file.write(book)
     return directory_path
 
+
+def save_image(image_url, id='', folder='static/images/'):
+    response = requests.get(image_url)
+    image = os.path.basename(image_url)
+    pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
+    directory_path = Path() / folder / image
+    with open(directory_path, 'wb') as file:
+        file.write(response.content)
+    return directory_path
 
 def check_for_redirect(response):
     response_history = response.history
@@ -47,8 +59,11 @@ def get_books(url, count):
         response.raise_for_status()
         if check_for_redirect(response):
             continue
-        title = get_title(url, book_id=_)
+        books_info = get_books_info(url, book_id=_)
+        title = books_info[0]
+        image = books_info[1]
         save_book(response.text, title, id=_)
+        save_image(image, id=_)
 
 
 def main():

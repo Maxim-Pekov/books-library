01aac2e361762dff86d3fcfadeb677484f45993f
Maxim-Pekov <max.pekov@gmail.com>
2022-11-01 23:40:39 +0300

    get more books with paginations

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -13,8 +13,14 @@ def get_books_by_category(response):
 
 def main():
     url = 'https://tululu.org/l55/'
-    response = requests.get(url)
-    get_books_by_category(response)
+    pages = range(1, 10)
+    for page in pages:
+        if page == 1:
+            url_page = url
+        else:
+            url_page = urljoin(url, str(page))
+        response = requests.get(url_page)
+        get_books_by_category(response)
 
 
 if __name__ == '__main__':

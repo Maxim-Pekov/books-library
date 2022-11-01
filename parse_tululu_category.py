12e23c0298afaa01fbe9acabe46db0c7020eb2cb
Maxim-Pekov <max.pekov@gmail.com>
2022-11-01 23:26:20 +0300

    get books url by category

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -3,17 +3,18 @@ from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
 
 
-def get_book_description(response):
+def get_books_by_category(response):
     soup = bs(response.text, 'lxml')
-    book_id_by_category = soup.find('div', class_='bookimage')
-    e = urljoin('https://tululu.org/', book_id_by_category.a['href'])
-    print(e)
+    books_by_category = soup.find_all('div', class_='bookimage')
+    for book in books_by_category:
+        book_url = urljoin('https://tululu.org/', book.a['href'])
+        print(book_url)
 
 
 def main():
     url = 'https://tululu.org/l55/'
     response = requests.get(url)
-    get_book_description(response)
+    get_books_by_category(response)
 
 
 if __name__ == '__main__':

51d6b6d21f2aa4af9dc524527e12c0b4b4c0d2dd
Maxim-Pekov <max.pekov@gmail.com>
2022-11-29 18:41:51 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,8 +1,9 @@
 import json
+import pathlib
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from pprint import pprint
-
-from more_itertools import chunked
+from itertools import count
+from more_itertools import chunked, ichunked
 from livereload import Server, shell
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
@@ -18,11 +19,15 @@ def follow_template_changes():
         books = json.load(fh)
     books = list(chunked(books, 2))
     pprint(books)
-    render_page = template.render(
-        books=books,
-    )
-    with open('index.html', 'w', encoding='utf-8') as file:
-        file.write(render_page)
+    all_chunks = ichunked(books, 10)
+    pprint(all_chunks)
+    pathlib.Path('pages').mkdir(parents=True, exist_ok=True)
+    for count, chunk in enumerate(all_chunks):
+        render_page = template.render(
+            books=chunk,
+        )
+        with open(f'pages/{count}index.html', 'w', encoding='utf-8') as file:
+            file.write(render_page)
 
 
 follow_template_changes()
@@ -32,4 +37,4 @@ follow_template_changes()
 
 server = Server()
 server.watch('base_template.html', follow_template_changes)
-server.serve(root='.')
+server.serve(root='./pages/0index.html')

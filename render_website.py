3f3a5d3ef5e8f8f8cbcfff24551bd5a2431d553e
Maxim-Pekov <max.pekov@gmail.com>
2022-12-03 17:01:39 +0300

    change README

--- render_website.py
+++ render_website.py
@@ -3,35 +3,56 @@ import math
 import pathlib
 from pathlib import Path
 from more_itertools import chunked, ichunked
-from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-directory_path = Path() / "media" / ''
 
-def follow_template_changes():
-    template = env.get_template('base_template.html')
-    with open('media/books.json', 'r', encoding='utf-8') as fh:
+BOOKS_JSON = Path() / "media" / 'books.json'
+NUMBER_OF_COLUMN = 2
+BOOK_PER_PAGES = 10
+PAGES_DIR = 'pages'
+TEMPLATE_HTML = 'base_template.html'
+
+
+def get_books_by_json():
+    with open(BOOKS_JSON, 'r', encoding='utf-8') as fh:
         books = json.load(fh)
-    books = list(chunked(books, 2))
-    all_chunks = ichunked(books, 10)
-    pathlib.Path('pages').mkdir(parents=True, exist_ok=True)
-    count_pages = range(1, math.ceil(len(books)/10) + 1)
-    for count, chunk in enumerate(all_chunks):
+    return books
+
+
+def split_by_columns_and_pages(elements, column=NUMBER_OF_COLUMN, pages=BOOK_PER_PAGES):
+    splited_by_column = list(chunked(elements, column))
+    splited_by_page = ichunked(splited_by_column, pages)
+    return splited_by_page
+
+
+def get_template_html(template_html):
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    return env.get_template(template_html)
+
+
+def render_html_pages(splited_books, books_count):
+    template = get_template_html(TEMPLATE_HTML)
+    count_pages = range(1, math.ceil(books_count / (BOOK_PER_PAGES * 2)) + 1)
+    for count, chunk in enumerate(splited_books):
         render_page = template.render(
             books=chunk,
             current_page=count + 1,
             count_pages=count_pages,
         )
-        with open(f'pages/index{count + 1}.html', 'w', encoding='utf-8') as file:
+        page_path = Path() / PAGES_DIR / f'index{count + 1}.html'
+        with open(page_path, 'w', encoding='utf-8') as file:
             file.write(render_page)
 
 
-follow_template_changes()
+def main():
+    books = get_books_by_json()
+    splited_books = split_by_columns_and_pages(books)
+    pathlib.Path(PAGES_DIR).mkdir(parents=True, exist_ok=True)
+    render_html_pages(splited_books, len(books))
 
-server = Server()
-server.watch('base_template.html', follow_template_changes)
-server.serve(root='pages/index0.html')
+
+if __name__ == '__main__':
+    main()
